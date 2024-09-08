c43d5b7bccbbc1976dbd26c4656edc496e7fc554
Amal T Scaria <amaltscaria22@gmail.com>
2024-09-08 17:05:27 +0530

    Add gitignore file

--- Starting Project/01-starting-project/src/App.jsx
+++ Starting Project/01-starting-project/src/App.jsx
@@ -1,10 +1,14 @@
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+const genRandomInt = (max) => Math.floor(Math.random()* (max + 1));
+
 const Header = () => {
+  const description = reactDescription[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
