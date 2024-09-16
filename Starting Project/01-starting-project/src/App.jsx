512ef87ad9c48834a359aa0d3e094882e6ff4d4c
Amal T Scaria <amaltscaria22@gmail.com>
2024-09-16 14:54:56 +0530

    Add core-concepts and examples component

--- Starting Project/01-starting-project/src/App.jsx
+++ Starting Project/01-starting-project/src/App.jsx
@@ -1,25 +1,48 @@
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-const genRandomInt = (max) => Math.floor(Math.random()* (max + 1));
+//commented the below image because header is now moved to a seperate component file under components
+// import img from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+// import jsxImg from "./assets/jsx-ui.png";
+// import stateImg from "./assets/state-mgmt.png";
 
-const Header = () => {
-  const description = reactDescription[genRandomInt(2)];
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-};
+// alternative syntax for using props
+import { CORE_CONCEPTS } from "./data";
+
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton/TabButton";
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+              image={componentImg}
+            ></CoreConcepts>
+            {/* alternative syntax for using props */}
+            <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            ></CoreConcepts>
+            <CoreConcepts {...CORE_CONCEPTS[2]}></CoreConcepts>
+            <CoreConcepts {...CORE_CONCEPTS[3]}></CoreConcepts>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Jsx</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
---
+++ Starting Project/01-starting-project/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,9 @@
+const TabButton = (props) => {
+  return (
+    <li>
+      <button>{props.children}</button>
+    </li>
+  );
+};
+
+export default TabButton;
